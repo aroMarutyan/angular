a7daf17a6b15b7ec913b79d4438dbfb0e7953b0c
Deathrovv <aro.maru7ian@gmail.com>
2022-04-06 01:56:03 +0200

    final commit

---
+++ angular-crash/src/app/services/task.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
+
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:6767/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+  // Middle of the road obserable solution
+  // getTasks(): Observable<Task[]> {
+  //   const tasks = of(TASKS);
+  //   return tasks;
+  // }
+  // Non-rxjs method
+  // getTasks(): Task[] {
+  //   return TASKS;
+  // }
+
+  updateToggleComplete(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+}
